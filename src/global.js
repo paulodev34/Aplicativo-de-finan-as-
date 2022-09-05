fb11c9888d6d431bab53360003c0ab9f952d637a
Billy paulinho <paulo_neal@hotmail.com>
2022-09-05 18:28:02 -0300

    APP finanças

---
+++ src/global.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+const Global = createGlobalStyle`
+
+*{
+    margin:0;
+    padding;0;
+}
+
+    body{
+        background-color: #2F4F4F
+    }
+`;
+
+export default Global;
