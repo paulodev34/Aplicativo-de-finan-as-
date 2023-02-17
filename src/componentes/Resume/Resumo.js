6401d8855bc292233b01235e117e02aac1ed7363
Billy paulinho <paulo_neal@hotmail.com>
2023-02-17 12:41:08 -0300

    Cards de entrada, saida e total

--- src/componentes/Resume/Resumo.js
+++ src/componentes/Resume/Resumo.js
@@ -1,11 +1,18 @@
 import React from "react";
 import * as C from "./style";
 import ResumeItem from "../ResumeItem";
+import {
+  FaRegArrowAltCircleUp,
+  FaRegArrowAltCircleDown,
+  FaDollarSign,
+} from "react-icons/fa";
 
 const Resumo = () => {
   return (
     <C.Container>
-      <ResumeItem />
+      <ResumeItem title="Entrada" Icon={FaRegArrowAltCircleUp} />
+      <ResumeItem title="Saída" Icon={FaRegArrowAltCircleDown} />
+      <ResumeItem title="Total" Icon={FaDollarSign} />
     </C.Container>
   );
 };
